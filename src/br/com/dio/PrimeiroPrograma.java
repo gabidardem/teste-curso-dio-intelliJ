6d6a12f473ec56c3f175118094c17aeb6458a883
gabidardem <gabidardem@gmail.com>
2022-05-30 21:07:45 -0300

    First commit.

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato; // Ctrl + Alt + o = limpa os imports que não estamos utilizando
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+//        Shift + F12 = deixar apenas o editor de texto
+//        Ctrl + Alt + L = arrumar o código
+
+//        Shift + F6 = Refatorar o nome
+
+//        Ctrl + D = duplica a linha
+//        Ctrl + Shift + Seta para cima/baixo = muda a linha de lugar
+//        Ctrl + Y = deleta linha
+
+//        Ctrl + Shift + / = comenta ou "descomenta" o que foi selecionado com /**/
+//        Ctrl + / = comenta ou "descomenta" o que foi selecionado com //
+
+        /*int a = 5, b = 4;
+        System.out.println("Hello, World! " + (a + b));*/
+
+    }
+}
